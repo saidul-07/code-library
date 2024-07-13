02c28e15fda8e24fe2aba2ea2b04c71702fd4a4e
saidul-07 <mdsayedul764@gmail.com>
2024-07-13 15:52:41 +0600

    Solution for CSES

---
+++ CSES_FI/Introductory Problems/1617_Bit_Strings.cpp
@@ -0,0 +1,56 @@
+                /********************************************************/
+                /********************************************************/
+                /**              BISMILLAHIR RAHMANIR RAHIM            **/
+                /**        Read the name of Allah, who created you     **/
+                /**                 MD. SAYEDUL ISLAM                  **/
+                /**   Department of Computer Science and Engineering   **/
+                /** Dhaka University of Engineering & Technology(DUET) **/
+                /**                                                    **/
+                /********************************************************/
+                /********************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define      fast()        ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define      ll            long long int
+#define      ull           unsigned long long int
+#define      mod           1000000007
+#define      vi            vector<int>
+#define      ivi(v)        for(auto & i:v)cin>>i
+#define      outvi(v)      for(auto & i:v)cout<<i<<' '
+#define      pb            push_back
+#define      fo(x,n)       for(int i=0; i<n; i++)
+#define      fo1(x,n)       for(int i=1; i<=n; i++)
+#define      endl          '\n'
+#define      yes           cout<<"YES"<<endl
+#define      no            cout<<"NO"<<endl
+#define      precision(n)  fixed<<setprecision(n)  //example cout<<precision(3)<<n<<endl;
+
+ll binary_exponentiation(int p){
+    ll res=1,base=2;
+    while(p){
+        if(p&1){
+            res=(res*base)%mod;
+            p--;
+            }
+        else{
+            base=(base*base)%mod;
+            p/=2;
+        }
+    }return res;
+}
+void solve(){
+    ll n; cin >> n;
+    cout<<binary_exponentiation(n)<<endl;
+}
+int main() {
+    fast();
+    //seive();
+    int t,i;t=1;
+   //cin>>t;
+    while(t--)solve();
+    return 0;
+}
+
+
+
